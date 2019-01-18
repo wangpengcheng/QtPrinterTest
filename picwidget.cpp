b301d46dbc1e392027ee72051bbd38c18528c696
wangpengcheng <37323208+wangpengcheng@users.noreply.github.com>
2019-01-18 15:02:59 +0800

    创建了QT文件，利用C++实现静默打印

---
+++ picwidget.cpp
@@ -0,0 +1,6 @@
+#include "picwidget.h"
+
+PicWidget::PicWidget()
+{
+
+}
---
+++ picwidget.h
@@ -0,0 +1,11 @@
+#ifndef PICWIDGET_H
+#define PICWIDGET_H
+
+
+class PicWidget
+{
+public:
+    PicWidget();
+};
+
+#endif // PICWIDGET_H
